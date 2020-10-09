442c8baa808973f3f88be547be4be22c5d28b6d7
Jose Alba <albajo0123@gmail.com>
2020-10-09 12:55:05 -0500

    Start of my speedrun

---
+++ flooringco/src/main/java/com/speedrun/flooringco/FlooringcoApplication.java
@@ -0,0 +1,13 @@
+package com.speedrun.flooringco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlooringcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlooringcoApplication.class, args);
+	}
+
+}
---
+++ flooringco/src/test/java/com/speedrun/flooringco/FlooringcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speedrun.flooringco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlooringcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
